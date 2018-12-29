1210952c046469f630efc114083ecb6f2c240166
Ulsacom <usmanovbekki@gmail.com>
2018-12-29 14:39:54 +0500

    new comit

--- app/src/main/java/com/ulsa/androidgit/MainActivity.java
+++ app/src/main/java/com/ulsa/androidgit/MainActivity.java
@@ -4,10 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public  static  final  float FLOAT=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
