abd70702f2924c0b89b95212307fab2670fd8a63
Ulsacom <usmanovbekki@gmail.com>
2019-03-02 10:19:02 +0500

    туц

--- app/src/main/java/com/ulsa/androidgit/MainActivity.java
+++ app/src/main/java/com/ulsa/androidgit/MainActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    public  static  final  float FLOAT=1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,3 +14,16 @@ public class MainActivity extends AppCompatActivity {
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
